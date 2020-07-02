32616c8f827b8c56b488c91b0cc14bef3f35b921
wlee221 <wlee221@live.unc.edu>
2020-07-01 18:59:43 -0700

    Initial import

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { withAuthenticator, ChatBot, SignOut } from 'aws-amplify-react'
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 function App() {
+  const handleComplete = (err, confirmation) => {
+    if (err) {
+      alert('Bot conversation failed')
+      return;
+    }
+
+    alert('Success: ' + JSON.stringify(confirmation, null, 2));
+    return 'Trip booked. Thank you! what would you like to do next?';
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Interaction Test
       </header>
+      <br />
+      <ChatBot
+        title="My Bot"
+        botName="BookTrip_dev"
+        welcomeMessage="Welcome, how can I help you today?"
+        onComplete={handleComplete}
+        clearOnComplete={true}
+        conversationModeOn={false}
+        voiceEnabled={true}
+      />
+      <SignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '@aws-amplify/ui/dist/style.css'
 
 ReactDOM.render(
   <React.StrictMode>
